f320f02b16982085492eac527be789b12a65e3cb
Lucijan Kvar <lk5628@student.uni-lj.si>
2016-06-02 10:28:35 +0000

    Dodajanje strani pomoc

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -49,6 +49,11 @@ $(document).ready(function() {
 		$("#preberiEHRid").val($(this).val());
 	});
 	
+	$('#user').change(function() {
+		$("#preberiSporocilo").html("");
+		$("#preberiUser").val($(this).val());
+	});
+	
 	$("#dodajPodatke").click(function(){
         $("#vnosPodatkov").css("display", "block");
     });
@@ -212,4 +217,66 @@ function dodajToCookie() {
     } else {
         return "";
     }
+}
+
+/**
+ * Za dodajanje vitalnih znakov pacienta je pripravljena kompozicija, ki
+ * vključuje množico meritev vitalnih znakov (EHR ID, datum in ura,
+ * telesna višina, telesna teža, sistolični in diastolični krvni tlak,
+ * nasičenost krvi s kisikom in merilec).
+ */
+function dodajMeritveVitalnihZnakov() {
+	sessionId = getSessionId();
+
+	var ehrId = $("#preberiUser").val();
+	var datumInUra = $("#dodajVitalnoDatumInUra").val();
+	var telesnaVisina = $("#dodajVitalnoTelesnaVisina").val();
+	var telesnaTeza = $("#dodajVitalnoTelesnaTeza").val();
+	var srcniUtrip = $("#dodajSrcniUtrip").val();
+
+
+	if (!ehrId || ehrId.trim().length == 0) {
+		$("#dodajMeritveVitalnihZnakovSporocilo").html("<span class='obvestilo " +
+      "label label-warning fade-in'>Prosim vnesite zahtevane podatke!</span>");
+	} else {
+		$.ajaxSetup({
+		    headers: {"Ehr-Session": sessionId}
+		});
+		var podatki = {
+			// Struktura predloge je na voljo na naslednjem spletnem naslovu:
+      // https://rest.ehrscape.com/rest/v1/template/Vital%20Signs/example
+		    "ctx/language": "en",
+		    "ctx/territory": "SI",
+		    "ctx/time": datumInUra,
+		    "vital_signs/height_length/any_event/body_height_length": telesnaVisina,
+		    "vital_signs/body_weight/any_event/body_weight": telesnaTeza,
+		   /*	"vital_signs/body_temperature/any_event/temperature|magnitude": telesnaTemperatura,
+		    "vital_signs/body_temperature/any_event/temperature|unit": "°C",
+		    "vital_signs/blood_pressure/any_event/systolic": sistolicniKrvniTlak,
+		    "vital_signs/blood_pressure/any_event/diastolic": diastolicniKrvniTlak,
+		    "vital_signs/indirect_oximetry:0/spo2|numerator": nasicenostKrviSKisikom */
+		};
+		var parametriZahteve = {
+		    ehrId: ehrId,
+		    templateId: 'Vital Signs',
+		    format: 'FLAT',
+		    committer: merilec
+		};
+		$.ajax({
+		    url: baseUrl + "/composition?" + $.param(parametriZahteve),
+		    type: 'POST',
+		    contentType: 'application/json',
+		    data: JSON.stringify(podatki),
+		    success: function (res) {
+		        $("#dodajMeritveVitalnihZnakovSporocilo").html(
+              "<span class='obvestilo label label-success fade-in'>" +
+              res.meta.href + ".</span>");
+		    },
+		    error: function(err) {
+		    	$("#dodajMeritveVitalnihZnakovSporocilo").html(
+            "<span class='obvestilo label label-danger fade-in'>Napaka '" +
+            JSON.parse(err.responseText).userMessage + "'!");
+		    }
+		});
+	}
 }
